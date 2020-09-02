5fa34003d3f1c9a01b7beb714f44bc30c77485cc
ayethida1931999 <ayethida@ucssittway.edu.mm>
2020-09-02 16:07:03 +0630

    welcome blade change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel git data update
                 </div>
 
                 <div class="links">
